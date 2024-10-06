80a334ece59aae05b8ee48a18212fcf92f848056
👨🏻‍💻 Progyan <bprogyan@gmail.com>
2024-10-06 13:57:19 +0530

    feat: Expose LockOption Interface for Extension
    Signed-off-by: Progyan Bhattacharya <bprogyan@gmail.com>

---
+++ .changeset/sixty-wasps-wave.md
@@ -0,0 +1,5 @@
+---
+"@theprodev/distributed-lock": minor
+---
+
+Expose Lock Option Interface for Application to Consume and Extend. Security and Patch upgrades for the dependencies.
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { DistributedLock, DistributedLockError } from "./distributed-lock";
+export type { LockOptions } from "./distributed-lock";
 export type { DistributedLockConfig } from "./redis.client";
