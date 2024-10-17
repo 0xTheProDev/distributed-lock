8227787eaed6984e044f084b255fee75a1cb4e4d
Progyan Bhattacharya <bprogyan@gmail.com>
2024-10-17 20:13:25 +0530

    fixup! fix(bytes): Use SHAsum for follow on Script Execution

--- scripts/setup-tests.ts
+++ scripts/setup-tests.ts
@@ -1,1 +1,13 @@
 process.env["TEST_ENV"] = "true";
+
+/**
+ * Mock `fs.readFile` to Avoid Actually Reading Server Scripts during Test.
+ */
+vi.mock("node:fs/promises", async (importOriginal) => {
+  const actual = (await importOriginal()) as object;
+
+  return {
+    ...actual,
+    readFile: () => Promise.resolve("FILE CONTENT"),
+  };
+});
